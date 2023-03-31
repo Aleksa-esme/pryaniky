e8e87962faa0bddc1e8c9edd858bee5d4679298e
Aleksa-esme <esme1990@mail.ru>
2023-03-31 13:11:01 +0300

    feat: router and protected routes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import { Login, Main } from 'pages';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material';
-import './App.css';
+import { Login, Main } from 'pages';
+import { ProtectedRoutes } from 'utils/ProtectedRoutes';
+
+export enum RouterPaths {
+  MAIN = '/',
+  LOGIN = '/login',
+}
 
 function App() {
   const theme = createTheme();
   return (
     <ThemeProvider theme={theme}>
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path={RouterPaths.LOGIN} element={<Login />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path={RouterPaths.MAIN} element={<Main />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
