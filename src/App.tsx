56f1bbedb918074a4553fb86f450bade15d7bc73
Aleksa-esme <esme1990@mail.ru>
2023-04-02 16:24:55 +0300

    fix: browserRouter to HashRouter for gh deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material';
 import { Login, Main } from 'pages';
 import { ProtectedRoutes } from 'utils/ProtectedRoutes';
@@ -12,14 +12,14 @@ function App() {
   const theme = createTheme();
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path={RouterPaths.LOGIN} element={<Login />} />
           <Route element={<ProtectedRoutes />}>
             <Route path={RouterPaths.MAIN} element={<Main />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   );
 }
